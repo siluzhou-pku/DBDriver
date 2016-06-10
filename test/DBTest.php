754d81fa79c5627bed170765fc4e1d83c0554115
siluzhou <1912317621@qq.com>
2016-06-10 21:47:41 +0800

    3.3.1.8
    编写DBTest类

--- test/DBTest.php
+++ test/DBTest.php
@@ -20,12 +20,28 @@ class DBTest extends PHPUnit_Framework_TestCase
         'quiet'         => 0,                   //安静模式 生产环境的
         'slowquery'     => 1,                   //对慢查询记录
     ];
+    private $db;
+    function setUp()
+    {
+        $this->db=new Lulu\DbDriver\Db($this->config);
+    }
     public function testGetOne()
     {
-        $db=new Db($this->config);
-        $sql="select truename from user where userId=1 ";
-        $this->assertEquals("4324", $db->getOne($sql));
 
+        $sql="select truename from user where userId=1 ";
+        $this->assertEquals("4324", $this->db->getOne($sql));
+
+    }
+    public function testInsert()
+    {
+        $res = $this->db->insert('user',[
+            'login'     => 'select',
+            'nickName'  => 'x7x658',
+            'password'  => '12345678',
+            'email'     => 'shampeak@sina.com',
+            'mobile'    => '13811069199',
+        ]);
+        $this->assertNotEquals(false,$res);
     }
 }
 
